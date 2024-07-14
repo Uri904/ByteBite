43c5d290913d8a147c9a10541f874eb88854a4bc
Uri904 <isai8.tapia@gmail.com>
2024-07-14 17:58:38 -0600

    ..

--- SQL/argumentosBD.php
+++ SQL/argumentosBD.php
@@ -3,7 +3,7 @@
     define('SERVIDOR','localhost');
     define('USUARIO','postgres');
     define("BASE","bytebite");
-    define("CONTRASEÑA","HolaQueHac3");
+    define("CONTRASEÑA","root");
     define("PUERTO","5432");
 
 ?>
