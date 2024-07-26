4411c73bba8a16e342789afc37a9bd7a1aed5762
Danie1XD <cumplidodaniel34@gmail.com>
2024-07-26 11:13:32 -0600

    ::

--- SQL/argumentosBD.php
+++ SQL/argumentosBD.php
@@ -3,7 +3,7 @@
     define('SERVIDOR','localhost');
     define('USUARIO','postgres');
     define("BASE","bytebite");
-    define("CONTRASEÑA","sao");
+    define("CONTRASEÑA","root");
     define("PUERTO","5432");
 
 ?>
